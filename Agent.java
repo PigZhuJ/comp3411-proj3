0d0e2d80edf98239cf23fcdcd48b1d416a534e96
saus-75 <georgechieng@gmail.com>
2017-05-11 18:23:09 +1000

    reformat andrew's code

--- Agent.java
+++ Agent.java
@@ -10,114 +10,58 @@ import java.io.*;
 import java.net.*;
 
 public class Agent {
-<<<<<<< HEAD
+    private List<State> stateList = new ArrayList<State>();
+    private char[][] exploredMap;
 
     public char get_action(char view[][]) {
-        //testing functionality
-//        int ch = 0;
-//        System.out.print("Enter Action(s): ");
-=======
-    
-   private List<State> stateList = new ArrayList<State>();
-   private char[][] exploredMap;
-   
-   public char get_action( char view[][] ) {
-<<<<<<< HEAD
+        char action;
+        // create a newState
+        State newState;
+        if (stateList.isEmpty()) {
+            newState = new State(false, view);
+        } else {
+            State prevState = stateList.get(stateList.size() - 1);
+            newState = new State(prevState.getHasKey(), view);
+        }
 
-       char action;
-       
-       // create a newState
-       State newState;
-       if (stateList.isEmpty()) {
-           newState = new State(false, view);
-       } else {
-           State prevState = stateList.get(stateList.size() - 1);
-           newState = new State(prevState.getHasKey(), view);
-       }
-       
-       if (newState.getHasKey()) {
-           buildExploredMap();
-       }
-       
-       if (view[1][2] == '~') {
-           action = 'r';
-       } else if (view[1][2] == '$') {
-           newState.setHasKey(true);
-           action = 'f';
-       } else {
-           action = 'f';
-           
-       }
+        if (newState.getHasKey()) {
+            buildExploredMap();
+        }
 
-       stateList.add(newState);
-       
-       return action;
-       
-   }
+        if (view[1][2] == '~') {
+            action = 'r';
+        } else if (view[1][2] == '$') {
+            newState.setHasKey(true);
+            action = 'f';
+        } else {
+            action = 'f';
 
-   private void buildExploredMap() {
-    
-       // the minimum and maximum values which the 
-       int xMin, xMax, yMin, yMax, xCurr, yCurr = 0;
-       
-       for (State checkState : stateList) {
-           if () {
-               
-           } else if () {
-               
-           } else if () {
-               
-           } else if () {
-               
-           }
-       }
-=======
-       //testing functionality
-       return ('f');
->>>>>>> 4a93b83e18a6d786b7dc4e1ff39b00666e9619f6
-//
-//        try {
-//            while (ch != -1) {
-//                // read character from keyboard
-//                ch = System.in.read();
-//
-//                switch (ch) { // if character is a valid action, return it
-//                    case 'F':
-//                    case 'L':
-//                    case 'R':
-//                    case 'C':
-//                    case 'U':
-//                    case 'B':
-//                    case 'f':
-//                    case 'l':
-//                    case 'r':
-//                    case 'c':
-//                    case 'u':
-//                    case 'b':
-//                        return ((char) ch);
-//                }
-//            }
-<<<<<<< HEAD
-//        } catch (IOException e) {
-//            System.out.println("IO error:" + e);
-//        }
+        }
+
+        stateList.add(newState);
+
+        return action;
+    }
+
+    private void buildExploredMap() {
+        // the minimum and maximum values which the
+        int xMin, xMax, yMin, yMax, xCurr, yCurr = 0;
+
+        for (State checkState : stateList) {
+            if () {
+
+            } else if () {
+
+            } else if () {
+
+            } else if () {
+
+            }
+        }
     }
 
     void print_view(char view[][]) {
         int i, j;
-=======
-//         }
-//      }
-//      catch (IOException e) {
-//         System.out.println ("IO error:" + e );
-//      }
->>>>>>> baf4df538ed584214e585a49de46481485155fc5
-   }
-
-void print_view( char view[][] ) {
-      int i,j;
->>>>>>> 4a93b83e18a6d786b7dc4e1ff39b00666e9619f6
-
         System.out.println("\n+-----+");
         for (i = 0; i < 5; i++) {
             System.out.print("|");
