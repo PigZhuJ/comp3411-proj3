59cc2ab1367cf2d803ce07d9c50bcb714643a9a1
saus-75 <georgechieng@gmail.com>
2017-05-25 21:10:03 +1000

    Debugging...

--- Agent.java
+++ Agent.java
@@ -114,7 +114,7 @@ public class Agent {
         System.out.println(sX + ", " + sY);
         System.out.println(lX + ", " + lY);
         for (Cood coKey : map.keySet()){
-            System.out.print(map.get(coKey));
+            System.out.print(coKey);
         }
 //        for (int i = sX; i < lX + 1; i++) {
 //            for (int j = sY; j < lY + 1; j++) {
