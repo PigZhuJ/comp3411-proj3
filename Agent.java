f3e38348764ab4f9e8a91528444df5d87db37a47
saus-75 <georgechieng@gmail.com>
2017-05-25 20:30:45 +1000

    Debugging...

--- Agent.java
+++ Agent.java
@@ -50,8 +50,8 @@ public class Agent {
                     map.put(newCood, newView[y][x]);
                     System.out.print(map.get(newCood));
                 }
-                System.out.println();
             }
+            System.out.println();
         }
     }
 
