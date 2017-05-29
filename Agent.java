0143af3e0bddeeabb763a98ceb884cf889d64c3e
andrew-ha <andrew.ha@live.com.au>
2017-05-29 10:17:27 +1000

    new debugging

--- Agent.java
+++ Agent.java
@@ -562,6 +562,7 @@ public class Agent {
     private char[][] rotate_view(char view[][], int times) {
         char newView[][] = view.clone();
         int temp = times;
+        System.out.println("direction = " + temp);
         // if the view is already upright, return the view as is
         if (temp == 0) {
             return view;
